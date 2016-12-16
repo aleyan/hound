5ff286ea9a1316b76dd7f2059ddf2d7d9b5ab616
Bruno Antunes <bruno@thoughtbot.com>
2016-12-16 16:06:10 +0000

    MOAR spex

--- app/assets/javascripts/components/__tests__/repos_container-test.jsx
+++ app/assets/javascripts/components/__tests__/repos_container-test.jsx
@@ -1,8 +1,6 @@
 import ReposContainer from '../repos_container.js';
 
 it('renders appropriately', () => {
-  const onRefreshClicked = jest.fn();
-
   const wrapper = shallow(
     <ReposContainer
       authenticity_token={"csrf_token"}
@@ -11,4 +9,18 @@ it('renders appropriately', () => {
     />
   );
   expect(wrapper).toMatchSnapshot();
+});
+
+it('sets the filterTerm appropriately on text input', () => {
+  const wrapper = mount(
+    <ReposContainer
+      authenticity_token={"csrf_token"}
+      has_private_access={false}
+      userHasCard={false}
+    />
+  );
+  const textInput = wrapper.find('.repo-search-tools-input');
+  textInput.simulate('change', {target: {value: "new text"}});
+
+  expect(wrapper.state('filterTerm')).toBe('new text');
 });
